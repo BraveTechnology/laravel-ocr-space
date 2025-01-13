6acf890b7d326b923357158305f07eeeddc24279
Thomas van der Westen <thomas@pixelpillow.nl>
2025-01-13 10:46:21 +0100

    Remove unused fromString method and update tests for OcrSpaceEngine

--- src/Enums/OcrSpaceEngine.php
+++ src/Enums/OcrSpaceEngine.php
@@ -6,18 +6,4 @@ enum OcrSpaceEngine: string
 {
     case Engine1 = '1';
     case Engine2 = '2';
-
-    /**
-     * Get the OcrSpaceEngine from a string value.
-     *
-     * @throws \ValueError
-     */
-    public static function fromString(string $value): OcrSpaceEngine
-    {
-        return match ($value) {
-            '1' => self::Engine1,
-            '2' => self::Engine2,
-            default => throw new \ValueError(sprintf('Invalid engine type "%s".', $value)),
-        };
-    }
 }
--- tests/Unit/Enums/OcrSpaceEngineTest.php
+++ tests/Unit/Enums/OcrSpaceEngineTest.php
@@ -1,11 +1,13 @@
 <?php
 
-it('can parse a engine type', function (): void {
+it('can parse a engine type 1', function (): void {
     $engine = \Codesmiths\LaravelOcrSpace\Enums\OcrSpaceEngine::from('1');
 
     expect($engine)->toBe(\Codesmiths\LaravelOcrSpace\Enums\OcrSpaceEngine::Engine1);
 });
 
-it('throws exception if engine type is invalid', function (): void {
-    dd(\Codesmiths\LaravelOcrSpace\Enums\OcrSpaceEngine::fromString('3'));
-})->throws(\ValueError::class, 'Invalid engine type "3".');
+it('can parse a engine type 2', function (): void {
+    $engine = \Codesmiths\LaravelOcrSpace\Enums\OcrSpaceEngine::from('2');
+
+    expect($engine)->toBe(\Codesmiths\LaravelOcrSpace\Enums\OcrSpaceEngine::Engine2);
+});
